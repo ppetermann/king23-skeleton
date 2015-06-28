314f371dd2a7a7d64e79f7311239a56c9188633b
Peter Petermann <peter.petermann@gameforge.com>
2015-06-28 15:57:22 +0200

    middleware for static files - not recommended for usage

--- conf/services/http-middleware.php
+++ conf/services/http-middleware.php
@@ -6,6 +6,7 @@ $container->register(
         /** @var \King23\Http\MiddlewareQueue $queue */
         $queue = $container->getInstanceOf(\King23\Http\MiddlewareQueue::class);
         $queue->addMiddleware(\King23\Http\Middleware\Whoops\Whoops::class);
+        $queue->addMiddleware(\Castle23\StaticFileMiddleware::class);
         $queue->addMiddleware(\King23\Http\RouterInterface::class);
         return $queue;
     }
